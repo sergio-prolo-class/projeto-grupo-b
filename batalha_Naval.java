f33b08961bfbba43842d987ec2255226b54020f1
GabrieleBuenoA <gabrielebueno.ifsc@gmail.com>
2025-12-03 01:08:15 +0000

    corrigido println

--- batalha_Naval.java
+++ batalha_Naval.java
@@ -4,19 +4,19 @@ import java.util.Random;
 public class batalha_Naval {
     public static void main(String[] args) {
 
-        int linhas  = 0;
+        int linhas  = 0 ;
         int colunas = 1 ;
+        
 
         while (linhas < 10) {
-            while (colunas < 10) {
+            while (colunas <= 10) {
                 System.out.printf(" . ");
                 colunas++;
             }
             linhas ++ ;
             colunas = 1 ;
-            System.out.println(" . ");
+            System.out.println();
 
         }
-
     }
 }
