945e43615417f86a9757bcef1227eb07f253fc51
GabrieleBuenoA <gabrielebueno.ifsc@gmail.com>
2025-12-03 00:51:30 +0000

    feito o tabuleiro 10x10 de pontos

---
+++ batalha_Naval.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class batalha_Naval {
+    public static void main(String[] args) {
+
+        int linhas  = 0;
+        int colunas = 1 ;
+
+        while (linhas < 10) {
+            while (colunas < 10) {
+                System.out.printf(" . ");
+                colunas++;
+            }
+            linhas ++ ;
+            colunas = 1 ;
+            System.out.println(" . ");
+
+        }
+
+    }
+}
