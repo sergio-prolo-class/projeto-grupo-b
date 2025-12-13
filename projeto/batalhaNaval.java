77f18e5fb93e9218c60fc9385d04ef33ea8663d3
Lucas Pacheco <lpachecokempfer@gmail.com>
2025-12-13 17:30:48 -0300

    Implementa criação e impressão usando metodos

--- projeto/batalhaNaval.java
+++ projeto/batalhaNaval.java
@@ -2,19 +2,32 @@ public class batalhaNaval {
 
 
     public static void main(String[] args) {
-        char[][] tabuleiro = new char[10][10];
+        // uso dos métodos
+        char[][] tabuleiro = criarTabuleiro(); 
+
+        imprimirTabuleiro(tabuleiro); 
+    }
+
+    public static char[][] criarTabuleiro(){
+        char[][] tabuleiro = new char[TAM][TAM];
 
         for (int linha = 0; linha < tabuleiro.length ; linha++) { 
             for (int coluna = 0; coluna < tabuleiro[0].length; coluna++) {
                 tabuleiro[linha][coluna] = '.';  //neste for o código seta o valor "." para cada posição no array, 
-                                                 //quando a coluna chega a 10 ele passa pra linha de baixo, setando o valor das mesmas 
-            }
+            }                                    //quando a coluna chega a 10 ele passa pra linha de baixo, setando o valor das mesmas 
         }
+
+        return tabuleiro;
+    }
+
+    public static void imprimirTabuleiro(char tabuleiro[][]){        
         for (int linha = 0; linha <  tabuleiro.length; linha++) {
             for (int coluna = 0; coluna < tabuleiro[0].length; coluna++) {
-                System.out.print(tabuleiro[linha][coluna] + " "); //neste for ele apenas imprime na tela todas as posições do array com o espaçamento
+                System.out.print(tabuleiro[linha][coluna] + " "); 
             }
-            System.out.println();
+            System.out.println(); //neste for ele apenas imprime na tela todas as posições do array com o espaçamento
         }
     }
+
+    static final int TAM = 10;
 }
